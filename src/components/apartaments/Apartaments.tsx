ef6743fb43dd9ebbc1be48afc4d298bfeaca0594
Alex <somersault97@gmail.com>
2022-03-15 17:26:52 +0300

    fix

--- src/components/apartaments/Apartaments.tsx
+++ src/components/apartaments/Apartaments.tsx
@@ -1,14 +1,14 @@
 import "./Apartaments.css"
 import ApartamentItem from "./ApartmentItem"
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import apartmentsSelectors from "../../redux/selectors/apartmentsSelectors";
-import { addRoom ,deleteRoom} from "../../redux/reducers/apartmentsReducer";
+import { addRoom, deleteRoom } from "../../redux/reducers/apartmentsReducer";
 import getRandomIntInclusive from "../../helpers/randomNumber";
 export interface RoomObject {
     image: string,
     description: string
 }
-function Apartaments () {
+function Apartaments() {
     const apartments = useSelector(apartmentsSelectors.getApartments);
     const RoomItems: Array<RoomObject> = apartments.apartments;
     const dispatch = useDispatch();
@@ -22,20 +22,20 @@ function Apartaments () {
                         item={item}
                         key={`${item.description}-${index}`} />)}
             </ul>
-            { RoomItems.length < 10 &&
-            <button 
-            type="button"
-            className="apartments-add_room"
-            onClick={()=> dispatch(addRoom(getRandomIntInclusive(0,RoomItems.length - 1)))}
-            >Добавить комнату</button>
-}
-            { RoomItems.length > 1 &&
-            <button
-            type="button"
-            className="apartments-delete_room"
-            onClick={()=> dispatch(deleteRoom(getRandomIntInclusive(0,RoomItems.length - 1)))}
-            >Удалить комнату</button>
-             }
+            {RoomItems.length < 10 &&
+                <button
+                    type="button"
+                    className="apartments-add_room"
+                    onClick={() => dispatch(addRoom(getRandomIntInclusive(0, RoomItems.length - 1)))}
+                >Добавить комнату</button>
+            }
+            {RoomItems.length > 1 &&
+                <button
+                    type="button"
+                    className="apartments-delete_room"
+                    onClick={() => dispatch(deleteRoom(getRandomIntInclusive(0, RoomItems.length - 1)))}
+                >Удалить комнату</button>
+            }
         </div>
     )
 }
